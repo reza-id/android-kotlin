155f4fb6167e2eac685fe337ca552ab0ccafc8da
Reza Agustiana <reza.agustiana@gmail.com>
2019-02-19 20:12:12 +0700

    Add menu for saveNote()

--- app/src/main/java/com/reza/latihan/EditNoteActivity.kt
+++ app/src/main/java/com/reza/latihan/EditNoteActivity.kt
@@ -2,11 +2,39 @@ package com.reza.latihan
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_edit_note.*
 
 class EditNoteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_note)
+
+        np_priority.minValue = 1
+        np_priority.maxValue = 10
+
+        title = "Add Note"
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_close)
+    }
+
+    private fun saveNote() {
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.save_note_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.save_note -> {
+                saveNote()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
