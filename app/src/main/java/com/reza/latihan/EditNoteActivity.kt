8276943b572c16545adcb7c881785551f82494d1
Reza Agustiana <reza.agustiana@gmail.com>
2019-02-19 20:10:51 +0700

    Create EditNoteActivity

---
+++ app/src/main/java/com/reza/latihan/EditNoteActivity.kt
@@ -0,0 +1,12 @@
+package com.reza.latihan
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class EditNoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_note)
+    }
+}
