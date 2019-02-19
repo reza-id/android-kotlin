2dc040e02228263cb76c0c91d7166b127cd7796c
Reza Agustiana <reza.agustiana@gmail.com>
2019-02-19 19:43:45 +0700

    Create Note Entity

---
+++ app/src/main/java/com/reza/latihan/Note.kt
@@ -0,0 +1,11 @@
+package com.reza.latihan
+
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(tableName = "note_table")
+data class Note(val title: String, val description: String, val priority: Int) {
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+}
