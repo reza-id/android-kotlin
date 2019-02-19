732a006880274bf77db31d8663d23fdf757f5a14
Reza Agustiana <reza.agustiana@gmail.com>
2019-02-19 20:00:36 +0700

    Connecting the ViewModel and the Activity

--- app/src/main/java/com/reza/latihan/MainActivity.kt
+++ app/src/main/java/com/reza/latihan/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.reza.latihan
 
-import android.support.v7.app.AppCompatActivity
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var noteViewModel: NoteViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel::class.java)
+        noteViewModel.allNotes.observe(this, Observer<List<Note>> {
+            // TODO: update RecyclerView
+            Toast.makeText(this@MainActivity, "Observer::onChanged", Toast.LENGTH_SHORT).show()
+        })
     }
 }
