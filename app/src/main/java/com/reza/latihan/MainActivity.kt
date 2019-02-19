dd2a9dfa1c6aad94fd0d818a31f3e62fe0498e45
Reza Agustiana <reza.agustiana@gmail.com>
2019-02-19 20:09:06 +0700

    Create the NoteAdapter

--- app/src/main/java/com/reza/latihan/MainActivity.kt
+++ app/src/main/java/com/reza/latihan/MainActivity.kt
@@ -4,7 +4,8 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,10 +15,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val adapter = NoteAdapter()
+
+        rv_notes.layoutManager = LinearLayoutManager(this)
+        rv_notes.setHasFixedSize(true)
+        rv_notes.adapter = adapter
+
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel::class.java)
         noteViewModel.allNotes.observe(this, Observer<List<Note>> {
-            // TODO: update RecyclerView
-            Toast.makeText(this@MainActivity, "Observer::onChanged", Toast.LENGTH_SHORT).show()
+            if (it != null)
+                adapter.setNotes(it)
         })
     }
 }
---
+++ app/src/main/java/com/reza/latihan/NoteAdapter.kt
@@ -0,0 +1,35 @@
+package com.reza.latihan
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.item_note.view.*
+
+
+class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteHolder>() {
+
+    private var notes: List<Note> = arrayListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteHolder =
+            NoteHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_note, parent, false))
+
+    override fun getItemCount() = notes.size
+
+    fun setNotes(notes: List<Note>) {
+        this.notes = notes
+        notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: NoteHolder, position: Int) {
+        holder.bindView(notes[position])
+    }
+
+    class NoteHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindView(note: Note) {
+            itemView.tv_title.text = note.title
+            itemView.tv_description.text = note.description
+            itemView.tv_priority.text = note.priority.toString()
+        }
+    }
+}
